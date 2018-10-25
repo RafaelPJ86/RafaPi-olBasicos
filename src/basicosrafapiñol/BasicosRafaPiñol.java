1dc4b27bbc995075f84db24eb5166b4ad81a23b8
usuario <usuario@192.168.1.142>
2018-10-25 23:20:20 +0200

    logico

--- src/basicosrafapiñol/BasicosRafaPiñol.java
+++ src/basicosrafapiñol/BasicosRafaPiñol.java
@@ -16,7 +16,22 @@ public class BasicosRafaPiñol {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-  
+        logicos();
+    }
+    public static void logicos(){
+        int x;
+        x= 8;
+        
+        if(x >=2 && x<=10){
+            
+           System.out.print("el valor esta entre 2 y 10");
+        
+        }else{
+        
+        System.out.print("el valor está fuera de rango");
+        
+        }
+    
     }
     
     
