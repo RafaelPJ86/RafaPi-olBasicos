365819a7251c2aff32ea7d8e7d78916941b6d6e0
usuario <usuario@192.168.1.142>
2018-10-25 22:47:24 +0200

    numericos

--- src/basicosrafapiñol/BasicosRafaPiñol.java
+++ src/basicosrafapiñol/BasicosRafaPiñol.java
@@ -15,9 +15,44 @@ public class BasicosRafaPiñol {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        numericos();
         // TODO code application logic here
   
     }
+    public static void numericos() {
+
+        //Estos son los tipos básicos
+        byte elMasPeque;//8bits
+        short chiquitin;//16 bits
+        int entero;//32 bits
+        long duplicaEntero;//64bits
+        float grande;//32bits
+        double doble;//64bits;
+
+        //Ejemplos de asignación de valores a las variables de tipos básicos
+        entero = 10;
+        doble = 5.6;
+        elMasPeque= 5;
+        chiquitin=9;
+        duplicaEntero=2000;
+        grande= 8;
+        
+
+        //Mostrar en pantalla el contendido de las variables de tipos básicos.
+        System.out.println("Valor entero");
+        System.out.println(entero);
+        System.out.println("Valor doble");
+        System.out.println(doble);
+        System.out.println("Valor elMasPeque");
+        System.out.println(elMasPeque);
+        System.out.println("Valor chiquitin");
+        System.out.println(chiquitin);
+        System.out.println("Valor duplicaEntero");
+        System.out.println(duplicaEntero);
+        System.out.println("Valor grande");
+        System.out.println(grande);
+    }
+    
     
     
 }
